3d2cca757889d81e8e61ab4b1bd04267093f9213
Dawid Halfar <halfardawid@gmail.com>
2017-11-15 14:17:29 +0100

    terrific performance upgrade

--- app/src/main/java/halfardawid/notepadx/activity/colorpalette/ColorGrid.java
+++ app/src/main/java/halfardawid/notepadx/activity/colorpalette/ColorGrid.java
@@ -1,6 +1,7 @@
 package halfardawid.notepadx.activity.colorpalette;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -39,14 +40,20 @@ public class ColorGrid extends ColorSliderGeneric {
     public void onDraw(Canvas c){
         int mx=c.getWidth();
         int my=c.getHeight();
-        Paint p=new Paint();
         int color=getColor();
         int alpha=Color.alpha(color);
         float[] hsv=new float[3];
         Color.colorToHSV(color,hsv);
-        for(int x=0;x<mx;x++,hsv[1]=(float)x/(float)mx)
-            for(int y=0;y<my;y++,hsv[2]=(float)y/(float)my,p.setColor(Color.HSVToColor(alpha,hsv)))
-                c.drawPoint(x,y,p);
+        float x_step=1f/(float)mx;
+        float y_step=1f/(float)my;
+        hsv[1]=0;
+        Bitmap b=Bitmap.createBitmap(mx,my, Bitmap.Config.ARGB_8888);
+        for(int x=0;x<mx;x++,hsv[1]+=x_step) {
+            hsv[2]=0;
+            for (int y = 0; y < my; y++, hsv[2] += y_step)
+                b.setPixel(x,y,Color.HSVToColor(alpha, hsv));
+        }
+        c.drawBitmap(b,0,0,null);
     }
 
     private int estimateColor(float s, float v) {
